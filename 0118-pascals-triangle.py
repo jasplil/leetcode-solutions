692005cacad708671728be9c136080bee158ab7d
jasmine <jasmine@jasmines-MacBook-Pro.local>
2019-08-22 12:33:39 +0800

    finished pascal triangle

---
+++ 0118-pascals-triangle.py
@@ -0,0 +1,10 @@
+def generate(self, numRows):
+	result = []
+	for i in range(numRows):
+		result.append([])
+		for j in range(i + 1):
+			if j in (0, i):
+				result[i].append(1)
+			else:
+				result[i].append(result[i - 1][j - 1] + result[i - 1][j])
+	return result
